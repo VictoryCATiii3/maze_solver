1be9bd61e80ffab66a4c4f6c77025fb3bc71a936
VictoryCATiii3 <victor7670236@gmail.com>
2025-02-27 16:36:25 -0800

    Finished up maze utilities

--- maze_util.py
+++ maze_util.py
@@ -1,5 +1,6 @@
 from tkinter import Tk, BOTH, Canvas
-import time
+import time, random
+from enum import Enum
 
 class Window:
     def __init__(self, width, height):
@@ -57,20 +58,31 @@ class Cell:
         self._y1 = top_left_point.y
         self._y2 = top_left_point.y + size
         self._win = window
+        self.visited = False
 
     def draw(self):
+        if self._win == None:
+            return
         left_line = Line(Point(self._x1, self._y1), Point(self._x1, self._y2))
         right_line = Line(Point(self._x2, self._y1), Point(self._x2, self._y2))
         top_line = Line(Point(self._x1, self._y1), Point(self._x2, self._y1))
         bottom_line = Line(Point(self._x1, self._y2), Point(self._x2, self._y2))
         if self.has_left_wall:
             left_line.draw_line(self._win.canvas, "black")
+        else:
+            left_line.draw_line(self._win.canvas, "white")
         if self.has_right_wall:
             right_line.draw_line(self._win.canvas, "black")
+        else:
+            right_line.draw_line(self._win.canvas, "white")
         if self.has_top_wall:
             top_line.draw_line(self._win.canvas, "black")
+        else:
+            top_line.draw_line(self._win.canvas, "white")
         if self.has_bottom_wall:
             bottom_line.draw_line(self._win.canvas, "black")
+        else:
+            bottom_line.draw_line(self._win.canvas, "white")
 
     def get_center(self):
         x = (self._x1 + self._x2)/2
@@ -78,19 +90,21 @@ class Cell:
         return Point(x, y)
 
     def draw_move(self, to_cell, undo=False):
-        color = "gray" if undo else "red"
+        color = "blue" if undo else "red"
         center1 = self.get_center()
         center2 = to_cell.get_center()
         move_line = Line(center1, center2)
         move_line.draw_line(self._win.canvas, color)
 
 class Maze:
-    def __init__(self, top_left_point, num_rows, num_collums, cell_size, win=None):
+    def __init__(self, top_left_point, num_rows, num_collums, cell_size, win=None, seed=None):
         self._win = win
         self.cells = self._create_cells(top_left_point, num_rows, num_collums, cell_size)
         self._top_left = top_left_point
         self._width = num_rows
         self._height = num_collums
+        if seed != None:
+            random.seed(seed)
         self._animate()
 
     def _create_cells(self, top_left_point, num_rows, num_collums, cell_size):
@@ -120,8 +134,141 @@ class Maze:
                 self._win.redraw()
                 time.sleep(0.05)
 
-    def _break_entrance_and_exit(self, i, j, side):
-        pass
+    def _break_wall(self, i, j, side):
+        match side:
+            case Side.TOP:
+                self.cells[i][j].has_top_wall = False
+            case Side.BOTTOM:
+                self.cells[i][j].has_bottom_wall = False
+            case Side.LEFT:
+                self.cells[i][j].has_left_wall = False
+            case Side.RIGHT:
+                self.cells[i][j].has_right_wall = False
+        self.cells[i][j].draw()
+
+    def _break_entrance_and_exit(self):
+        self._break_wall(0, 0, Side.TOP)
+        self._break_wall(-1, -1, Side.BOTTOM)
+
+    def generate_maze(self):
+        goal_i, goal_j = len(self.cells)-1, len(self.cells[0])-1
+        visited = [(0, 0)]
+        while visited != []:
+            self._break_walls_r(visited, goal_i, goal_j)
+
+    def _break_walls_r(self, visited, goal_i, goal_j):
+        if not visited:
+            return
+        current = visited.pop(-1)
+        i, j = current[0], current[1]
+        self.cells[i][j].visited = True
+        neighbors = self.get_free_neighbors(i,j)
+        if not neighbors:
+            return
+        go_to = random.choice(neighbors)
+        i2, j2 = None, None
+        match go_to:
+            case Side.TOP:
+                i2, j2 = i, j-1
+                self._break_wall(i, j, Side.TOP)
+                self._break_wall(i2, j2, Side.BOTTOM)
+            case Side.BOTTOM:
+                i2, j2 = i, j+1
+                self._break_wall(i, j, Side.BOTTOM)
+                self._break_wall(i2, j2, Side.TOP)
+            case Side.LEFT:
+                i2, j2 = i-1, j
+                self._break_wall(i, j, Side.LEFT)
+                self._break_wall(i2, j2, Side.RIGHT)
+            case Side.RIGHT:
+                i2, j2 = i+1, j
+                self._break_wall(i, j, Side.RIGHT)
+                self._break_wall(i2, j2, Side.LEFT)
+        if (i2, j2) == (goal_i, goal_j):
+            return
+        visited.append((i, j))
+        visited.append((i2, j2))
+        self._break_walls_r(visited, goal_i, goal_j)
+        return
+
+    def get_free_neighbors(self, i, j):
+        cell = self.cells[i][j]
+        neighbors = []
+        if i > 0 and not self.cells[i-1][j].visited:
+            neighbors.append(Side.LEFT)
+        if j > 0 and not self.cells[i][j-1].visited:
+            neighbors.append(Side.TOP)
+        if i < len(self.cells)-1 and not self.cells[i+1][j].visited:
+            neighbors.append(Side.RIGHT)
+        if j < len(self.cells[0])-1 and not self.cells[i][j+1].visited:
+            neighbors.append(Side.BOTTOM)
+        return neighbors
+
+    def _reset_cells_visited(self):
+        for column in self.cells:
+            for cell in column:
+                cell.visited = False
+
+    def get_moves(self, i, j):
+        cell = self.cells[i][j]
+        neighbors = self.get_free_neighbors(i, j)
+        moves = []
+        if Side.TOP in neighbors and not cell.has_top_wall:
+            moves.append(Side.TOP)
+        if Side.RIGHT in neighbors and not cell.has_right_wall:
+            moves.append(Side.RIGHT)
+        if Side.BOTTOM in neighbors and not cell.has_bottom_wall:
+            moves.append(Side.BOTTOM)
+        if Side.LEFT in neighbors and not cell.has_left_wall:
+            moves.append(Side.LEFT)
+        return moves
+
+    def solve(self):
+        self._reset_cells_visited()
+        goal_i, goal_j = len(self.cells)-1, len(self.cells[0])-1
+        at_end = False
+        visited = [(0, 0)]
+        test_result = self._solve_r(visited, goal_i, goal_j)
+
+    def _solve_r(self, visited, goal_i, goal_j):
+        self._win.redraw()
+        time.sleep(0.05)
+        if visited == []:
+            print("visited empty at start of _solve_r")
+            return False
+        (i, j) = visited.pop(-1)
+        if (i, j) == (goal_i, goal_j):
+            return True
+        self.cells[i][j].visited = True
+        moves = self.get_moves(i, j)
+        if not moves:
+            (i_last, j_last) = visited[-1]
+            self.cells[i][j].draw_move(self.cells[i_last][j_last], undo=True)
+            return self._solve_r(visited, goal_i, goal_j)
+        go_to = random.choice(moves)
+        moves.remove(go_to)
+        i2, j2 = None, None
+        match go_to:
+            case Side.TOP:
+                i2, j2 = i, j-1
+            case Side.RIGHT:
+                i2, j2 = i+1, j
+            case Side.BOTTOM:
+                i2, j2 = i, j+1
+            case Side.LEFT:
+                i2, j2 = i-1, j
+        self.cells[i][j].draw_move(self.cells[i2][j2])
+        if (i2, j2) == (goal_i, goal_j):
+            return True
+        visited.append((i, j))
+        visited.append((i2, j2))
+        return self._solve_r(visited, goal_i, goal_j)
+
+class Side(Enum):
+    TOP = "top"
+    BOTTOM = "bottom"
+    LEFT = "left"
+    RIGHT = "right"
 
 def main():
     win = Window(800, 600)
@@ -137,9 +284,10 @@ def main():
     #Test drawing path
     test_cell.draw_move(test_cell2)
     test_cell2.draw_move(test_cell3, undo=True)
-
     maze = Maze(Point(90, 10), 10, 8, 10, win)
-
+    maze._break_entrance_and_exit()
+    maze.generate_maze()
+    maze.solve()
     win.wait_for_close()
 
 if __name__=="__main__":
--- test.py
+++ test.py
@@ -4,12 +4,9 @@ from maze_util import Window, Maze, Point
 
 class Tests(unittest.TestCase):
     def test_maze_create_cells(self):
-        print("hello world")
-        test_window = Window(200, 200)
         num_rows = 12
         num_cols =10
         m1 = Maze(Point(0,0), num_rows, num_cols, 10)
-        print(len(m1.cells))
         self.assertEqual(
             len(m1.cells),
             num_rows
@@ -18,3 +15,27 @@ class Tests(unittest.TestCase):
             len(m1.cells[0]),
             num_cols
         )
+
+    def test_break_entrence_and_exit(self):
+        num_rows = 4
+        num_cols = 6
+        m2 = Maze(Point(1, 1), num_rows, num_cols, 5)
+        m2._break_entrance_and_exit()
+        self.assertEqual(m2.cells[0][0].has_top_wall, False)
+        self.assertEqual(m2.cells[0][0].has_left_wall, True)
+        self.assertEqual(m2.cells[3][5].has_bottom_wall, False)
+        self.assertEqual(m2.cells[3][5].has_right_wall, True)
+
+    def test_reset_cells_visited(self):
+        num_rows = 5
+        num_cols = 5
+        m3 = Maze(Point(0,0), num_rows, num_cols, 5)
+        m3.generate_maze()
+        self.assertEqual(m3.cells[0][0].visited, True)
+        self.assertEqual(m3.cells[-1][-1].visited, False) # We finish here so it isn't marked as visited.
+        self.assertEqual(m3.cells[2][2].visited, True)
+        m3._reset_cells_visited() # All cells should once again be marked unvisited
+        self.assertEqual(m3.cells[0][0].visited, False)
+        self.assertEqual(m3.cells[-1][-1].visited, False) # End should remain false
+        self.assertEqual(m3.cells[2][2].visited, False)
+
